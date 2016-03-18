468ef5b1086aaa2ef81fdf4cda8e9dbefff80e7f
Dmitriy Sevastuk <dmitriy793@gmail.com>
2016-03-18 05:02:38 +0300

    1.0
    Попытка №1 выполнить данное 13.03.2016  задание.

---
+++ Translator/Translator/ViewModel/TranslatePageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    class TranslatePageViewModel
+    {
+        public ICommand StartListenUserSpeech { get; set; }
+
+        private bool canExecute = true;
+
+        public void ChangeCanExecute(object obj)
+        {
+            canExecute = !canExecute;
+        }
+
+        public string ButtonContent
+        {
+            get
+            {
+                return " Start listen\nuser speach";
+            }
+        }
+
+        public void CallListenUserSpeach(object obj)
+        {
+            Model.Translator.ListenUserSpeach(obj.ToString());
+        }
+
+        public TranslatePageViewModel()
+        {
+            StartListenUserSpeech = new Command.StartListenUserSpeechCommand(CallListenUserSpeach, param => this.canExecute);
+        }
+    }
+}
