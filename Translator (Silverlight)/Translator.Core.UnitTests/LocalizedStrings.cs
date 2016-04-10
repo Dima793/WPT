89fb70acd3aa5f238000df10a25ce7095140df0e
Константин Новокрещенов < >
2016-04-11 00:31:27 +0300

    Translator for Windows Phone Silverlight

---
+++ Translator (Silverlight)/Translator.Core.UnitTests/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Translator.Core.UnitTests.Resources;
+
+namespace Translator.Core.UnitTests
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
